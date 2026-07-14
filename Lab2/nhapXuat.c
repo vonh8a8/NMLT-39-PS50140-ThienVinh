2cc380cd23fe9ddf1f82dd059f76fa7345dad531
Vinh <thvinh2543@gmail.com>
2026-07-14 11:19:50 +0700

    lab 2

---
+++ Lab2/nhapXuat.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int namSinh;;
+    printf("Vui long cho biet nam sinh cua ban: ");
+    scanf("%d", &namSinh);
+    printf("Nam nay ban da %d tuoi\n", 2026 - namSinh);
+    return 0;
+}

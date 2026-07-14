16a045338482676509e809e7eaf0c44dbe545482
Vinh <thvinh2543@gmail.com>
2026-07-14 09:50:07 +0700

    lab1

--- Lab1/Bai3_tinhDiem.c
+++ Lab1/Bai3_tinhDiem.c
@@ -1,15 +1,41 @@
 #include <stdio.h>
-int main() {
-	
-	float diemTB, diemToan, diemLy, diemHoa;
-	printf("Nhap diem Toan: ");
-	scanf("%f", &diemToan);
-	printf("Nhap diem Ly: ");
-	scanf("%f", &diemLy);
-	printf("Nhap diem Hoa: ");
-	scanf("%f", &diemHoa);
-	diemTB = (diemToan + diemLy + diemHoa) / 3.0;// ep kieu tu so nguyen sang so thuc
-	printf("Diem trung binh: %.2f\n", diemTB);
-	return 0;
+#include <string.h>
+#include <time.h> //chạy chương trình tính tuổi
+int main()
+{
+    int toan, ly, hoa;
+    char HoVaTen[50];
+    char MSSV[10];
+    unsigned short Namsinh;
+
+    printf("Nhap ho va ten: ");
+    scanf("%[^\n]", HoVaTen);
+
+    printf("Nhap MSSV: ");
+    scanf("%s", MSSV);
+    getchar();
+
+    printf("Nhap nam sinh: ");
+    scanf("%hu", &Namsinh);
+
+    printf("Nhap diem mon Toan: ");
+    scanf("%d", &toan);
+
+    printf("Nhap diem mon Ly: ");
+    scanf("%d", &ly);
+
+    printf("Nhap diem mon Hoa: ");
+    scanf("%d", &hoa);
+
+    printf("\nThong tin sinh vien:\n");
+    printf("Ho va ten: %s\n", HoVaTen);
+    printf("MSSV: %s\n", MSSV);
+    printf("Nam sinh: %hu\n", Namsinh);
+    printf("Tuoi: %d\n", 2026 - Namsinh);
+    printf("Diem Toan: %d\n", toan);
+    printf("Diem Ly: %d\n", ly);
+    printf("Diem Hoa: %d\n", hoa);
+    printf("Diem trung binh: %.2f\n", (toan + ly + hoa) / 3.0);
+    return 0;
 
 }

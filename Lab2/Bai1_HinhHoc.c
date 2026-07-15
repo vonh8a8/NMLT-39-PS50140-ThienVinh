087cb99cc60a490b82981721bcedb9d946166bfc
Vinh <thvinh2543@gmail.com>
2026-07-15 22:12:52 +0700

    Hoan thanh Lab 2 Bai 1

--- Lab2/Bai1_HinhHoc.c
+++ Lab2/Bai1_HinhHoc.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
+#define PI 3.14159
 int main (){
-    double dai, rong, chuvi, dientich;
+    //tính chu vi và diện tích hình chữ nhật
+    double dai, rong, chuVi, dienTich;
     printf("Nhap chieu dai: ");
     scanf("%lf", &dai);
     printf("Nhap chieu rong: ");
     scanf("%lf", &rong);
-    chuvi = 2 * (dai + rong);
-    dientich = dai * rong;
-    printf("Chu vi hinh chu nhat la: %.2lf\n", chuvi);
-    printf("Dien tich hinh chu nhat la: %.2lf\n", dientich);
+    chuVi = 2 * (dai + rong);
+    dienTich = dai * rong;
+    printf("Chu vi hinh chu nhat la: %.2lf\n", chuVi);
+    printf("Dien tich hinh chu nhat la: %.2lf\n", dienTich);
 
+    //tính chu vi và diện tích hình tròn
+    double banKinh, chuVi_tron, dienTich_tron;
+    printf("Nhap ban kinh hinh tron: ");
+    scanf("%lf", &banKinh);
+    chuVi_tron = 2 * PI * banKinh;
+    dienTich_tron = PI * banKinh * banKinh;
+    printf("\nChu vi hinh tron la: %.2lf\n", chuVi_tron);
+    printf("Dien tich hinh tron la: %.2lf\n", dienTich_tron);
+    printf("\nChu vi hinh tron la: %.2lf\n", banKinh * 2 * PI);
+    printf("Dien tich hinh tron la: %.2lf\n", banKinh * banKinh * PI);
     return 0;
 }

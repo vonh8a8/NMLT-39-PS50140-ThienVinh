97b192c826289fa34a1e0e8e8c99b8a8a163adfe
Vinh <thvinh2543@gmail.com>
2026-07-30 11:17:57 +0700

    Hoan thanh Chuc nang 4 - Xay dung ham checkTriangle phan loai tam giac

--- Lab5/Main_Menu_Lab5.c
+++ Lab5/Main_Menu_Lab5.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <locale.h>
 void menu();
 void chucNang1();
 int soLonNhat(int a, int b, int c);
 void chucNang2();
 bool kiemTraNamNhuan(int nam);
 void chucNang3();
+void hoanVi(int *a, int *b);
 void chucNang4();
-void chucNang5();
+const char *phanLoaiTamGiac(float a, float b, float c);
 int main()
 {
-    setlocale(LC_ALL, "Vietnamese");
     int chon;
     do
     {
         menu();
-        printf(">> Xin mời chọn chức năng (1-5): ");
+        printf("Xin moi chon chuc nang (1-5): ");
         scanf("%d", &chon);
         switch (chon)
         {
@@ -33,10 +32,10 @@ int main()
             chucNang4();
             break;
         case 5:
-            chucNang5();
+            printf("Thoat chuong trinh.\n");
             break;
         default:
-            printf("Lựa chọn không hợp lệ. Vui lòng chọn lại.\n");
+            printf("Lua chon khong hop le. Vui long chon lai.\n");
             break;
         }
     } while (chon != 5);
@@ -44,23 +43,23 @@ int main()
 }
 void menu()
 {
-    printf("+---------------------------------------------------+");
-    printf("|             MENU CHƯƠNG TRÌNH LAB 5               |");
-    printf("+---------------------------------------------------+");
-    printf("| 1. Tìm giá trị lớn nhất trong 3 số                |");
-    printf("| 2. Kiểm tra Năm nhuận                             |");
-    printf("| 3. Hoán vị 2 số (Sử dụng Con trỏ)                 |");
-    printf("| 4. Kiểm tra & Phân loại Tam giác                  |");
-    printf("| 5. Thoát chương trình                             |");
-    printf("+---------------------------------------------------+");
+    printf("+---------------------------------------------------+\n");
+    printf("|             MENU CHUONG TRINH LAB 5               |\n");
+    printf("+---------------------------------------------------+\n");
+    printf("| 1. Tim gia tri lon nhat trong 3 so                |\n");
+    printf("| 2. Kiem tra nam nhuan                             |\n");
+    printf("| 3. Hoan vi 2 so (Su dung Con tro)                 |\n");
+    printf("| 4. Kiem tra & Phan loai Tam giac                  |\n");
+    printf("| 5. Thoat chuong trinh                             |\n");
+    printf("+---------------------------------------------------+\n");
 }
-
+// Chuc nang 1: Tim gia tri lon nhat trong 3 so
 void chucNang1()
 {
     int a, b, c;
-    printf("Nhập 3 số nguyên: ");
+    printf("Nhap a, b, c: ");
     scanf("%d %d %d", &a, &b, &c);
-    printf("Giá trị lớn nhất trong 3 số là: %d\n", soLonNhat(a, b, c));
+    printf("Gia tri lon nhat trong 3 so vua nhap la: %d\n", soLonNhat(a, b, c));
 }
 
 int soLonNhat(int a, int b, int c)
@@ -76,19 +75,20 @@ int soLonNhat(int a, int b, int c)
     }
     return max;
 }
-
+// Chuc nang 2: kiem tra nam nhuan
 void chucNang2()
 {
     int nam;
-    printf("Nhập năm: ");
+    printf("Chuc nang 2: Kiem tra nam nhuan\n");
+    printf("Nhap nam: ");
     scanf("%d", &nam);
     if (kiemTraNamNhuan(nam))
     {
-        printf("Năm %d là năm nhuận.\n", nam);
+        printf("Nam %d la nam nhuan.\n", nam);
     }
     else
     {
-        printf("Năm %d không phải là năm nhuận.\n", nam);
+        printf("Nam %d khong phai la nam nhuan.\n", nam);
     }
 }
 
@@ -100,18 +100,72 @@ bool kiemTraNamNhuan(int nam)
     }
     return false;
 }
-
+// Chuc nang 3: Hoan vi 2 so (Su dung Con tro)
 void chucNang3()
 {
-    printf("Chức năng 3: Hoán vị 2 số (Sử dụng Con trỏ)\n");
+    int a, b;
+    printf("Chuc nang 3: Hoan vi 2 so (Su dung Con tro)\n");
+    printf("Nhap 2 so nguyen: ");
+    scanf("%d %d", &a, &b);
+    printf("Truoc khi hoan vi: a = %d, b = %d\n", a, b);
+    hoanVi(&a, &b);
+    printf("Sau khi hoan vi: a = %d, b = %d\n", a, b);
 }
 
+void hoanVi(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+// Chuc nang 4: Kiem tra & Phan loai Tam giac
 void chucNang4()
 {
-    printf("Chức năng 4: Kiểm tra & Phân loại Tam giác\n");
+    printf("Chuc nang 4: Kiem tra & Phan loai Tam giac\n");
+    float a, b, c;
+    printf("Nhap 3 canh cua tam giac: ");
+    scanf("%f %f %f", &a, &b, &c);
+    const char *loai = phanLoaiTamGiac(a, b, c);
+    printf("Tam giac vua nhap la: %s\n", loai);
 }
 
-void chucNang5()
+const char *phanLoaiTamGiac(float a, float b, float c)
 {
-    printf("Chức năng 5: Thoát chương trình\n");
+    // Tam giác đều: a == b và b == c.
+    // Tam giác vuông: a^2 + b^2 == c^2 hoặc a^2 + c^2 == b^2 hoặc b^2 + c^2 == a^2.
+    // Tam giác cân: a == b hoặc a == c hoặc b == c.
+    // Tam giác vuông cân: Vừa thỏa mãn điều kiện Vuông vừa thỏa mãn Cân.
+    // Tam giác thường: Các trường hợp còn lại.
+
+    if (a + b > c && a + c > b && b + c > a && a > 0 && b > 0 && c > 0) //&& a-b < c && a-c < b && b-c < a
+    {
+        if (a == b && b == c)
+        {
+            return "Tam giac deu";
+        }
+        else if (a == b || a == c || b == c)
+        {
+            return "Tam giac can";
+        }
+        else if ((a * a + b * b == c * c) || (a * a + c * c == b * b) || (b * b + c * c == a * a))
+        {
+            if (a == b || a == c || b == c)
+            {
+                return "Tam giac vuong can";
+            }
+            else
+            {
+                return "Tam giac vuong";
+            }
+        }
+        else
+        {
+            return "Tam giac thuong";
+        }
+    }
+    else
+    {
+        return "Khong phai tam giac";
+    }
+    return "Khong phai bo 3 canh cua tam giac";
 }

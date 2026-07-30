80eadc33079c0ce0116c2ae16884621f285fcabe
Vinh <thvinh2543@gmail.com>
2026-07-30 10:26:08 +0700

    Lab5: Dung khung Menu va khai bao cac nguyen mau ham (Function Prototypes)

---
+++ Lab5/Main_Menu_Lab5.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdbool.h>
+void menu();
+void chucNang1();
+int soLonNhat(int a, int b, int c);
+void chucNang2();
+bool kiemTraNamNhuan(int nam);
+void chucNang3();
+void chucNang4();
+void chucNang5();
+int main()
+{
+    int chon;
+    do
+    {
+        menu();
+        printf(">> Xin mời chọn chức năng (1-5): ");
+        scanf("%d", &chon);
+        switch (chon)
+        {
+        case 1:
+            chucNang1();
+            break;
+        case 2:
+            chucNang2();
+            break;
+        case 3:
+            chucNang3();
+            break;
+        case 4:
+            chucNang4();
+            break;
+        case 5:
+            chucNang5();
+            break;
+        default:
+            printf("Lựa chọn không hợp lệ. Vui lòng chọn lại.\n");
+            break;
+        }
+    } while (chon != 5);
+    return 0;
+}
+void menu()
+{
+    printf("+---------------------------------------------------+");
+    printf("|             MENU CHƯƠNG TRÌNH LAB 5               |");
+    printf("+---------------------------------------------------+");
+    printf("| 1. Tìm giá trị lớn nhất trong 3 số                |");
+    printf("| 2. Kiểm tra Năm nhuận                             |");
+    printf("| 3. Hoán vị 2 số (Sử dụng Con trỏ)                 |");
+    printf("| 4. Kiểm tra & Phân loại Tam giác                  |");
+    printf("| 5. Thoát chương trình                             |");
+    printf("+---------------------------------------------------+");
+}
+
+void chucNang1()
+{
+    int a, b, c;
+    printf("Nhập 3 số nguyên: ");
+    scanf("%d %d %d", &a, &b, &c);
+    printf("Giá trị lớn nhất trong 3 số là: %d\n", soLonNhat(a, b, c));
+}
+
+int soLonNhat(int a, int b, int c)
+{
+    int max = a;
+    if (b > max)
+    {
+        max = b;
+    }
+    if (c > max)
+    {
+        max = c;
+    }
+    return max;
+}
+
+void chucNang2()
+{
+    int nam;
+    printf("Nhập năm: ");
+    scanf("%d", &nam);
+    if (kiemTraNamNhuan(nam))
+    {
+        printf("Năm %d là năm nhuận.\n", nam);
+    }
+    else
+    {
+        printf("Năm %d không phải là năm nhuận.\n", nam);
+    }
+}
+
+bool kiemTraNamNhuan(int nam)
+{
+    return false;
+}
